2fefecb81114469004f64da32308c8a9dabdfe8c
Maysha-Tabassum <mayshatabassum77@gmail.com>
2022-10-09 07:19:25 +0600

    orders page added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,21 +1,49 @@
 import React, { useEffect, useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import { addToDb, deleteShoppingCart, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
+    const products = useLoaderData();
     const [cart, setCart] = useState([]);
 
-        useEffect(()=>{
-        fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    }, [])
-    const handleAddToCart =(product)=> {
-        console.log(product);
-        const newCart = [...cart, product];
+    const clearCart = () =>{
+        setCart([]);
+        deleteShoppingCart();
+    }
+
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        // console.log(storedCart);
+        const savedCart = [];
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products])
+
+    const handleAddToCart =(selectedProduct)=> {
+        console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
         setCart(newCart);
+        addToDb(selectedProduct.id);
     }
     return (
         <div className='shopping-container'>
@@ -29,7 +57,11 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart clearCart={clearCart} cart={cart}>
+                <Link to='/orders'>
+                        <button>Review Order</button>
+                    </Link>
+                </Cart>
             </div>
         </div>
     );
